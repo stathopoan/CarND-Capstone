1a34e960dc4b88d81862205b6d0f9e2e36a9bbf4
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2017-11-06 21:01:48 +0100

    Added traffic lights visualisation (with status) to the chart node

--- ros/src/tools/chart.py
+++ ros/src/tools/chart.py
@@ -2,10 +2,11 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane
+from styx_msgs.msg import Lane, TrafficLightArray
 from matplotlib import pyplot as plt
 from threading import Lock
 import copy
+import itertools
 
 
 class Chart(object):
@@ -33,6 +34,11 @@ class Chart(object):
         self.is_path_updated = False
         self.path_lock = Lock()
 
+        # The traffic lights, status and coordinates, i.e. the /vehicle/traffic_lights coming from the simulator
+        self.traffic_lights = None
+        self.traffic_lights_lock = Lock()
+
+        # Subscription to /current_pose, /final_waypoints and /vehicle/traffic_lights happens in waypoints_cb
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         self.loop()
@@ -69,6 +75,12 @@ class Chart(object):
         self.path_lock.release()
         return updated
 
+    def get_traffic_lights(self):
+        self.traffic_lights_lock.acquire()
+        tls = copy.deepcopy(self.traffic_lights)
+        self.traffic_lights_lock.release()
+        return tls
+
     def loop(self):
         rate = rospy.Rate(5)  # The chart refresh rate, no need for it to be stupid high
 
@@ -90,12 +102,14 @@ class Chart(object):
         first_iter = True
         first_path_iter = True
         update_chart = True
+        first_tl_iter = True
+        tl_marker = ('ro','yo', 'go', 'bo')
         while not rospy.is_shutdown():
             # Update the car pose
             if self.pose_updated():
                 x, y = self.get_pose()
                 if first_iter:
-                    car_dot, = self.ax.plot(x, y, 'ro')
+                    car_dot, = self.ax.plot(x, y, 'ko')
                     first_iter = False
                 else:
                     car_dot.set_ydata(y)
@@ -109,11 +123,28 @@ class Chart(object):
                     path_wp_x.append(wp.pose.pose.position.x)
                     path_wp_y.append(wp.pose.pose.position.y)
                     if first_path_iter:
-                        path_line, = self.ax.plot(path_wp_x, path_wp_y, 'y', linewidth=3.)
+                        path_line, = self.ax.plot(path_wp_x, path_wp_y, 'm', linewidth=3.)
                         first_path_iter = False
                     else:
                         path_line.set_ydata(path_wp_y)
                         path_line.set_xdata(path_wp_x)
+                    update_chart = True
+            # Update the traffic lights
+            if self.traffic_lights is not None:
+                tls = self.get_traffic_lights()
+                tl_x, tl_y, tl_c = [], [], []
+                for tl in tls:
+                    tl_x.append(tl[0])
+                    tl_y.append(tl[1])
+                    tl_c.append(tl_marker[tl[2]])
+                marker = itertools.cycle(tl_c)
+                if first_tl_iter:
+                    tl_dots, = self.ax.plot(tl_x, tl_y, marker.next())
+                    first_tl_iter = False
+                    update_chart = True
+                else:
+                    tl_dots.set_markerfacecolor(marker.next()[0])
+                    tl_dots.set_markeredgecolor(marker.next()[0])
                     update_chart = True
             # If any update was applied to the chart, then redraw it
             if update_chart:
@@ -128,9 +159,10 @@ class Chart(object):
         self.waypoints = waypoints.waypoints
         self.waypoints_lock.release()
         rospy.logdebug('Received {} waypoints:'.format(len(waypoints.waypoints)))
-        # rospy.logdebug(waypoints)
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/final_waypoints', Lane, self.path_cb)
+        rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray , self.traffic_lights_cb)
+
 
     def pose_cb(self, msg):
         self.pose_lock.acquire()
@@ -146,7 +178,12 @@ class Chart(object):
         self.is_path_updated = True
         self.path_lock.release()
 
-
+    def traffic_lights_cb(self, msg):
+        self.traffic_lights_lock.acquire()
+        self.traffic_lights = []
+        for tl in msg.lights:
+            self.traffic_lights.append((tl.pose.pose.position.x, tl.pose.pose.position.y, tl.state))
+        self.traffic_lights_lock.release()
 
 if __name__ == '__main__':
     try:
